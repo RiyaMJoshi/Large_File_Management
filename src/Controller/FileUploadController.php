e3a1fdfec2326debd5cf1272d9a09831718ef911
Sakshi Goraniya <sakshigoraniya13@gmail.com>
2022-05-02 14:57:01 +0530

    csv file upload

--- src/Controller/FileUploadController.php
+++ src/Controller/FileUploadController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\FileUploader;
 
 class FileUploadController extends AbstractController
 {
@@ -14,5 +16,27 @@ class FileUploadController extends AbstractController
         return $this->render('file_upload/index.html.twig', [
             'controller_name' => 'FileUploadController',
         ]);
+    }
+    #[Route('/uploadfile', name: 'app_upload_file')]
+    public function upload(Request $request)
+    {
+        //dd($request->files->get('file'));
+
+        // dd($request);
+        // die;
+        $file=$request->files->get('formFile');
+        $uploads_directory=$this->getParameter('uploads_directory');
+
+        $extension = $file->guessExtension();
+       // dd($extension);
+        // if ($extension === 'csv') {
+        //     # code...
+        // }
+        $filename=md5(uniqid()).'.'.$file->guessExtension();
+        $file->move(
+            $uploads_directory,
+            $filename
+        );
+        return new Response("file upload success");
     }
 }
