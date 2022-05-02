94d998c52b1d0f6dfe6d42031f450d5481fbcd96
Riya Joshi <riyajoshi312@gmail.com>
2022-05-02 18:48:03 +0530

    Getting CSV filenames from Zip sucessfully

--- src/Controller/FileUploadController.php
+++ src/Controller/FileUploadController.php
@@ -3,52 +3,71 @@
 namespace App\Controller;
 
 use App\Entity\MetaTable;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry;
+use ZipArchive;
 
 class FileUploadController extends AbstractController
 {
     #[Route('/', name:'app_homepage')]
-function index(): Response
+    function index(): Response
     {
-    return $this->render('file_upload/index.html.twig', [
-        'controller_name' => 'FileUploadController',
-    ]);
-}
-#[Route('/uploadfile', name:'app_upload_file')]
-function upload(Request $request, ManagerRegistry $doctrine)
-    {
-        //get and upload csv
-    $file = $request->files->get('formFile');
-    $uploads_directory = $this->getParameter('uploads_directory');
-    $filename = md5(uniqid()) . '.' . $file->guessExtension();
-    $file->move(
-        $uploads_directory,
-        $filename
-    );
- 
-    $file_full = $uploads_directory . '/' . $filename;
-    // Open and extract csv
-    $filesize = filesize($file_full); // bytes
-    $filesize = round($filesize / 1024,2);
-    if (($handle = fopen($file_full, "r")) !== false) {
-        $columns = fgetcsv($handle, 1000, ",");
-        fclose($handle);
+        return $this->render('file_upload/index.html.twig', [
+            'controller_name' => 'FileUploadController',
+        ]);
     }
 
-    //save to meta table in db
-    
-    $metaTable = new MetaTable();
-    $em = $doctrine->getManager();
-    $metaTable->setFilename($filename);
-    $metaTable->setFilesize($filesize);
-    $metaTable->setColumns($columns);
-    $em->persist($metaTable);
-    $em->flush();
+    #[Route('/uploadfile', name:'app_upload_file')]
+    function upload(Request $request, ManagerRegistry $doctrine)
+    {
+        //Get and Upload CSV
+        $file = $request->files->get('formFile');
+        $uploads_directory = $this->getParameter('uploads_directory');
 
-    return new Response("file upload success");
-}
+        // Extract file if it is zip
+        if ($file->guessExtension() == 'zip') {
+            $zipArchive = new ZipArchive();
+            $zipArchive->open($file);
+
+            //dd($file);
+            if ($zipArchive) {
+                // Get only the first available CSV File from the Zip
+                $stat = $zipArchive->statIndex(0);
+                //$file = basename($stat['name']);
+                var_dump(basename($stat['name']));
+            }
+        }
+        die();
+
+        $filename = md5(uniqid()) . '.' . $file->guessExtension();
+        //$filename = md5(uniqid()) . '.' . 'csv';
+        $file->move(
+            $uploads_directory,
+            $filename
+        );
+
+        $file_full = $uploads_directory . '/' . $filename;
+        // Open and extract csv
+        $filesize = filesize($file_full); // bytes
+        $filesize = round($filesize / 1024, 2);
+        if (($handle = fopen($file_full, "r")) !== false) {
+            $columns = fgetcsv($handle, 1000, ",");
+            fclose($handle);
+        }
+
+        //save to meta table in db
+
+        $metaTable = new MetaTable();
+        $em = $doctrine->getManager();
+        $metaTable->setFilename($filename);
+        $metaTable->setFilesize($filesize);
+        $metaTable->setColumns($columns);
+        $em->persist($metaTable);
+        $em->flush();
+
+        return new Response("file upload success");
+    }
 }
